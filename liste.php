4b3d76a148c1a8ef4ae5f0559e3dba67dbdcde45
nicoodu77 <nicoodu77@hotmail.fr>
2016-04-06 06:28:20 -0400

    liste debut

---
+++ liste.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>hello world en php !</title>
+  </head>
+  <body>
+<?php
+$rep = array(
+  array("Nom"=>"Moteur de recherche"),
+	array("Nom"=>"Google","image"=>"","Url"=>"http://www.google.fr"),
+  array("Nom"=>"Yahoo","image"=>"","Url"=>"https://fr.yahoo.com"),
+  array("Nom"=>"Journaux"),
+  array("Nom"=>"LE Monde","image"=>"","Url"=>"http://www.lemonde.fr"),
+  array("Nom"=>"Linux","image"=>"","Url"=>""),
+  array("Nom"=>"L'Equipe","image"=>"","Url"=>""),
+  array("Nom"=>"","image"=>"","Url"=>""),
+  array("Nom"=>"","image"=>"","Url"=>"")
+  );
+?>
+  </body>
+</html>
